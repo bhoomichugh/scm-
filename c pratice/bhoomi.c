d345d9d6abd26e3a50003324dc678f9920d6900e
bhoomichugh <149398945+bhoomichugh@users.noreply.github.com>
2024-05-07 22:44:14 +0530

    edit

--- c pratice/bhoomi.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    char arr[5] = {'a', 'b', 'c', 'd', 'e'};
-    printf("%c", arr[2]);
-
-
-    return 0;
-}
--- c pratice/stringbasic.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    char ch = 'A';
-    printf("%d", ch);
-
-
-    return 0;
-}
